a7471305a7739be305845a22c0d01bacaae768cb
JoabTorres <joabtorres1508@gmail.com>
2014-12-26 18:16:30 -0200

    estrutura basica montata

---
+++ paginas/basico/todos.php
@@ -0,0 +1,69 @@
+<?php require("../subpaginas/header.php") ?>
+
+    <!-- fim do menu -->
+    <section class="container-fluid" id="conteudo-site">
+    	<div class="row">
+    		<div class="col-lg-12 col-md-12">
+    			<h1 class="titulo-categoria">Todos os modelos básicos</h1>
+    			<div class="row">
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="/imagens/test.png" alt="...">
+				      <div class="caption">
+				      	<a href="#" class="btn btn-default btn-categoria" role="button">Site 1</a></p>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="/imagens/test2.png" alt="...">
+				      <div class="caption">
+				      	<a href="#" class="btn btn-default btn-categoria" role="button">Site 2</a></p>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="/imagens/test3.png" alt="...">
+				      <div class="caption">
+				      	<a href="#" class="btn btn-default btn-categoria" role="button">Site 3</a></p>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+    		</div>
+    	</div>
+
+		<div class="row">
+    		<div class="col-lg-12 col-md-12">
+    			<div class="row">
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="/imagens/test.png" alt="...">
+				      <div class="caption">
+				      	<a href="#" class="btn btn-default btn-categoria" role="button">Site 1</a></p>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="/imagens/test2.png" alt="...">
+				      <div class="caption">
+				      	<a href="#" class="btn btn-default btn-categoria" role="button">Site 2</a></p>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="/imagens/test3.png" alt="...">
+				      <div class="caption">
+				      	<a href="#" class="btn btn-default btn-categoria" role="button">Site 3</a></p>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+    		</div>
+    	</div>
+    </section>
+
+ <?php require("../subpaginas/footer.php") ?>
---
+++ paginas/master/todos.php
@@ -0,0 +1,40 @@
+<?php require("../subpaginas/header.php") ?>
+
+    <!-- fim do menu -->
+    <section class="container-fluid" id="conteudo-site">
+    	<div class="row">
+    		<div class="col-lg-12 col-md-12">
+    			<h1 class="titulo-categoria">Todos os modelos master</h1>
+    			<div class="row">
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="/imagens/test.png" alt="...">
+				      <div class="caption">
+				      	<a href="#" class="btn btn-default btn-categoria" role="button" target="_blank">Site 1</a></p>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="/imagens/test2.png" alt="...">
+				      <div class="caption">
+				      	<a href="#" class="btn btn-default btn-categoria" role="button" target="_blank" >Site 2</a></p>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="col-sm-4 col-md-4">
+				    <div class="thumbnail">
+				      <img src="/imagens/test3.png" alt="...">
+				      <div class="caption">
+				      	<a href="#" class="btn btn-default btn-categoria" role="button" target="_blank" target="_blank">Site 3</a></p>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+    		</div>
+    	</div>
+
+		
+    </section>
+
+ <?php require("../subpaginas/footer.php") ?>
